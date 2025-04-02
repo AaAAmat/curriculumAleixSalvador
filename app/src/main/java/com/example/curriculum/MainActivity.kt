b2ceb19d1d2cc8005a9a90100da03900bf03f860
AaAAmat <asalvador.smx@institutcampalans.net>
2025-04-02 15:51:48 +0200

    currriculum 2

--- app/src/main/java/com/example/curriculum/MainActivity.kt
+++ app/src/main/java/com/example/curriculum/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.example.curriculum
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,16 +49,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CurriculumTheme {
-                curriculum()
-            }
+            ViewContainer()
         }
     }
 }
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
 @Composable
+fun ViewContainer(){
+    Scaffold(
+        topBar = {Toolbar()},
+        content = {curriculum()}
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Toolbar() {
+    TopAppBar(
+        title = { Text(text = "Aleix's Curriculum", color = colorResource(id = R.color.white)) },
+        actions = {
+            FAB()
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = colorResource(id = R.color.background),
+            titleContentColor = colorResource(id = R.color.white)
+        )
+    )
+}
+
+@Composable
+fun FAB(){
+    val context = LocalContext.current
+
+    FloatingActionButton(onClick = {
+        Toast.makeText(context, "Funcionalitat en construcció", Toast.LENGTH_SHORT).show()
+    }){
+        Text(text = "Aleix")
+    }
+}
+
+@Composable
 fun curriculum(){
+var Ccounter by rememberSaveable {mutableStateOf(0)}
+var JScounter by rememberSaveable {mutableStateOf(0)}
+var Jcounter by rememberSaveable {mutableStateOf(0)}
+var Kcounter by rememberSaveable {mutableStateOf(0)}
+var Scounter by rememberSaveable {mutableStateOf(0)}
+
+
     LazyColumn (
         modifier = Modifier
             .fillMaxSize()
@@ -47,7 +108,8 @@ fun curriculum(){
     {
         item {
             Image(
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .height(300.dp)
                     .padding(bottom = 20.dp),
                 painter = painterResource(id = R.drawable.caracurriculum),
@@ -60,7 +122,9 @@ fun curriculum(){
                 textAlign = TextAlign.Center
                 )
             LazyRow(horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 5.dp)
             ){
                 item{
                     Text(text = "Telèfon:",
@@ -86,10 +150,47 @@ fun curriculum(){
             Text(
                 text = "GitHub: https://github.com/AaAAmat",
                 fontSize = 18.sp,
-                modifier = Modifier.padding(20.dp).fillMaxWidth(),
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
                 color = Color.Blue,
                 textAlign = TextAlign.Center
             )
+            Row(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = "C#:",
+                    modifier = Modifier.clickable{ Ccounter++ })
+                Text(text = Ccounter.toString(),
+                    modifier = Modifier.padding(start = 4.dp))
+            }
+            Row(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = "JavaScript:",
+                    modifier = Modifier.clickable{ JScounter++ })
+                Text(text = JScounter.toString(),
+                    modifier = Modifier.padding(start = 4.dp))
+            }
+            Row(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = "Java:",
+                    modifier = Modifier.clickable{ Jcounter++ })
+                Text(text = Jcounter.toString(),
+                    modifier = Modifier.padding(start = 4.dp))
+            }
+            Row(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = "Kotlin:",
+                    modifier = Modifier.clickable{ Kcounter++ })
+                Text(text = Kcounter.toString(),
+                    modifier = Modifier.padding(start = 4.dp))
+            }
+            Row(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = "Swift:",
+                    modifier = Modifier.clickable{ Scounter++ })
+                Text(text = Scounter.toString(),
+                    modifier = Modifier.padding(start = 4.dp))
+            }
         }
     }
 }
